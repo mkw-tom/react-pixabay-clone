22d3166f498d99f846ebe2cb0209c684021c027b
Tomu Maekawa <tomu.m1107@icloud.com>
2024-03-06 10:31:34 +0900

    apiを叩きデータをプロップス経由で渡し、map関数で展開して表示させてみた。

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { useRef, useState } from 'react';
 import './App.css';
 import ImageGallery from './ImageGallery';
 
 function App() {
+  const [fetchData, setFetchData] = useState([]);
+  const ref = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(ref.current.value);
+    const endpointURL = `https://pixabay.com/api/?key=42517536-94329ae0feda69ba424375dc5&q=${ref.current.value}&image_type=photo&pretty=true`;
+    fetch(endpointURL)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data.hits);
+        setFetchData(data.hits);
+      })
+  }
+
   return (
     <div className="container">
       <h2>My Pixabay</h2>
-      <form>
-        <input type='text' placeholder='画像を探す'/>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type='text' placeholder='画像を探す' ref={ref}/>
       </form>
-      <ImageGallery />
+      <ImageGallery fetchData={fetchData} />
     </div>
   );
 }
--- src/ImageGallery.jsx
+++ src/ImageGallery.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import "./ImageGallery.css"
 
-const ImageGallery = () => {
+const ImageGallery = ({ fetchData }) => {
   return (
     <div>
-      <div className="imgage-wrapper">
-        <div className="image">
-          <img src="" alt="" />
-        </div>
+      <div className="images-wrapper">
+        {fetchData.map((data) => (
+          <div className="image" key={data.id}>
+            <a href={data.pageURL} target='_blank'>
+              <img src={data.largeImageURL} alt="" />
+            </a>
+            
+          </div>
+        ))}
       </div>
     </div>
   )
